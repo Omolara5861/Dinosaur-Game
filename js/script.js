33d31efc7ffa3fc65359e816cf6a085bc4104f08
Lara <debbiegterra@gmail.com>
2021-12-23 12:54:18 +0100

    Made code responsive with Js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Code to fix scaling Issues to make the game fully responsive 
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 30;
+const worldElem = document.querySelector("[data-world]");
+
+setPixelToWorldScale()
+window.addEventListener("resize", setPixelToWorldScale);
+
+
+function setPixelToWorldScale() {
+    let worldToPixelScale;
+    //if the window screen is wide, the pixels scale would be based on the width  
+    if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH;
+    }
+    //if the window screen is narrow, the pixels scale would be based on the Height
+    else {
+    worldToPixelScale = window.innerHeight / WORLD_HEIGHT;
+    }
+    //Updating the Width and Height value depending on the screensize
+    worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
+    worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
+}
+
